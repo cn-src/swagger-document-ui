9b333c86775628ae3a8e101a3fcf359f3143036b
cn-src <public@javaer.cn>
2018-08-24 21:30:40 +0800

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -18,5 +18,7 @@ module.exports = {
     ],
     testMatch: [
         '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-    ]
+    ],
+    testURL: 'http://localhost/',
+    cache: false
 };
