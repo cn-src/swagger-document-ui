ef1d3e54158575b67546ad54302ce5fd141d1688
cn-src <public@javaer.cn>
2018-08-26 23:30:49 +0800

    根据 package.json 生成 pom.xml

---
+++ generatePom.js
@@ -0,0 +1,20 @@
+var fs = require("fs");
+
+var pg = JSON.parse(fs.readFileSync('package.json', {encoding: 'UTF-8'}));
+var data = fs.readFileSync('pom.template.xml', {encoding: 'UTF-8'});
+
+data = data.replace(/{{artifactId}}/g, pg.name);
+data = data.replace(/{{version}}/g, pg.version);
+
+const contributor = pg.contributors[0].split(' ');
+data = data.replace(/{{developer-id}}/g, contributor[0]);
+data = data.replace(/{{developer-name}}/g, contributor[0]);
+data = data.replace(/{{developer-email}}/g, contributor[1].substring(1, contributor[1].length - 1));
+
+data = data.replace(/{{license.name}}/g, pg.license);
+data = data.replace(/{{repo-git.url}}/g, pg.repository);
+data = data.replace(/{{repo.url}}/g, pg.repository.substring(0, pg.repository.length - 4));
+
+fs.writeFileSync('pom.xml', data)
+
+// console.log("同步读取: " + data);
