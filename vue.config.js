2008557ffc66493dca1d72ea89155972522e50a5
cn-src <public@javaer.cn>
2018-08-26 23:58:07 +0800

    修改构建参数

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 module.exports = {
+    filenameHashing: false,
+    pages: {
+        'swagger-document-ui': {
+            // page 的入口
+            entry: 'src/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'API 文档',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        }
+    },
+
     chainWebpack: config => {
         config.module
             .rule('vue')
@@ -12,4 +30,4 @@ module.exports = {
             })
             .end()
     }
-}
+};
