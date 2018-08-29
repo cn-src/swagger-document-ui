be49985b828728e7224057dc01916f0afcc57c1c
cn-src <public@javaer.cn>
2018-08-29 12:59:21 +0800

    reformat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/strongly-recommended',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'vue/attribute-hyphenation': 'warning',
-    'vue/html-indent': 'warning',
-    'vue/max-attributes-per-line': 'off',
-    'vue/no-parsing-error': 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/strongly-recommended',
+        'eslint:recommended'
+    ],
+    rules: {
+        'no-console': 'off',
+        'no-debugger': 'off',
+        'vue/attribute-hyphenation': 'warning',
+        'vue/html-indent': 'warning',
+        'vue/max-attributes-per-line': 'off',
+        'vue/no-parsing-error': 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 };
