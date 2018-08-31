4abf5b84e9411e20e876b29c76915e5ee9ac7c06
cn-src <public@javaer.cn>
2018-08-31 16:22:31 +0800

    开发首页信息

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,47 @@
 <template>
-  <div>Home</div>
+  <div>
+    <Divider>{{ apiTitle }}</Divider>
+    <Table :columns="apiInfoColumns" :data="apiInfo" border
+           :show-header="false"/>
+  </div>
 </template>
 
 <script>
+    import store from '@/store'
+
     export default {
-        name: "Home"
+        name: "Home",
+        data() {
+            return {
+                apiInfoColumns: [
+                    {title: '', key: 'k1', width: 110, align: 'right'},
+                    {title: '', key: 'k2'}
+                ]
+            }
+        },
+        computed: {
+            apiTitle() {
+                if (!store.state.apiData || !store.state.apiData.info) {
+                    return ''
+                }
+                return store.state.apiData.info.title;
+            },
+            apiInfo() {
+                if (!store.state.apiData || !store.state.apiData.info) {
+                    return []
+                }
+                const info = store.state.apiData.info;
+                return [
+                    {k1: '描述:', k2: info.description},
+                    {k1: '版本:', k2: info.version},
+                    {k1: '许可证:', k2: info.license.name},
+                    {k1: '服务条款:', k2: info.termsOfService},
+                    {k1: '维护人:', k2: info.contact},
+                    {k1: '维护人邮箱:', k2: info.contact.email},
+                    {k1: '接口地址:', k2: info.title}
+                ]
+            }
+        }
     }
 </script>
 
