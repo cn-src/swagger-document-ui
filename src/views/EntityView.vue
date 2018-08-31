ac1db063535a16a5848abbe13362579e845fb8df
cn-src <public@javaer.cn>
2018-09-01 00:29:34 +0800

    调整锚点

--- src/views/EntityView.vue
+++ src/views/EntityView.vue
@@ -2,8 +2,8 @@
   <div style="padding: 24px 0 0 24px;">
     <Tabs>
       <TabPane label="API 文档" icon="md-document">
-        <div id="doc-content" :style="{height: '75vh',overflowY: 'auto',paddingBottom: '100px'}">
-          <div :style="{width:'75vw'}">
+        <div id="doc-content" :style="{height: '75vh',overflowY: 'auto',paddingBottom: '100px', paddingRight: '200px'}">
+          <div>
             <ul>
               <li><h2 id="h2_1">接口说明</h2>
               </li>
@@ -71,7 +71,7 @@
               </template>
             </ul>
           </div>
-          <div style="top:100px;left:90%;position: fixed;">
+          <div style="top:100px; right: 20px; position: fixed; width: 180px">
             <Anchor show-ink container="#doc-content">
               <AnchorLink href="#h2_1" title="接口说明"/>
               <AnchorLink href="#h2_2" title="请求参数">
