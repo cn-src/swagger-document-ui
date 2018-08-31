83de295454ad0d48bdc4e2f20ba485289d8b93b6
cn-src <public@javaer.cn>
2018-08-31 17:46:43 +0800

    开发首页信息

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
             return {
                 apiInfoColumns: [
                     {title: '', key: 'k1', width: 110, align: 'right'},
-                    {title: '', key: 'k2'}
+                    {title: '', key: 'k2', render: methodColumnRender}
                 ]
             }
         },
@@ -31,17 +31,32 @@
                     return []
                 }
                 const info = store.state.apiData.info;
+
                 return [
                     {k1: '描述:', k2: info.description},
                     {k1: '版本:', k2: info.version},
-                    {k1: '许可证:', k2: info.license.name},
+                    {k1: '许可证:', k2: info.license},
                     {k1: '服务条款:', k2: info.termsOfService},
                     {k1: '维护人:', k2: info.contact},
                     {k1: '维护人邮箱:', k2: info.contact.email},
-                    {k1: 'host:', k2:info.host},
-                    {k1: 'basePath:', k2:info.basePath}
+                    {k1: 'host:', k2: info.host},
+                    {k1: 'basePath:', k2: info.basePath}
                 ]
             }
+        }
+    }
+
+    function methodColumnRender(h, params) {
+        if (params.row.k1 === '许可证:') {
+            const license = params.row.k2;
+            return h('a', {
+                attrs: {
+                    target: '_blank',
+                    href: license.url
+                }
+            }, license.name);
+        } else {
+            return h('span', {}, params.row.k2)
         }
     }
 </script>
