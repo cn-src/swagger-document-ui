baced2856f1e1db2f3883f4573ceeb7e796baef7
cn-src <public@javaer.cn>
2018-08-31 14:52:43 +0800

    rename

--- src/router.js
+++ src/router.js
@@ -26,7 +26,7 @@ export default new Router({
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/EntityView.vue')
+            component: () => import(/* webpackChunkName: "entity-view" */ './views/EntityView.vue')
         }
     ]
 })
