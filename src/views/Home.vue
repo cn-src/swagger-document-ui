1f0567bedfafbb725a5b660e89cbe74487c2bc51
cn-src <public@javaer.cn>
2018-09-02 20:37:22 +0800

    完善首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,8 +37,7 @@
                     {k1: '版本:', k2: info.version},
                     {k1: '许可证:', k2: info.license},
                     {k1: '服务条款:', k2: info.termsOfService},
-                    {k1: '维护人:', k2: info.contact},
-                    {k1: '维护人邮箱:', k2: info.contact.email},
+                    {k1: '联系开发人员:', k2: info.contact.email},
                     {k1: 'host:', k2: info.host},
                     {k1: 'basePath:', k2: info.basePath}
                 ]
@@ -55,7 +54,23 @@
                     href: license.url
                 }
             }, license.name);
-        } else {
+        }
+        else if (params.row.k1 === '服务条款:') {
+            return h('a', {
+                attrs: {
+                    target: '_blank',
+                    href: params.row.k2
+                }
+            }, params.row.k2);
+        } else if (params.row.k1 === '联系开发人员:') {
+            return h('a', {
+                attrs: {
+                    target: '_blank',
+                    href: 'mailto:'+params.row.k2
+                }
+            }, params.row.k2);
+        }
+        else {
             return h('span', {}, params.row.k2)
         }
     }
