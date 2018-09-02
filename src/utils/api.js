71eb3c8d2d6ccca3afcfbae0f61dbd70aece4c23
cn-src <public@javaer.cn>
2018-09-02 21:40:30 +0800

    实现分组功能

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,12 +5,12 @@ import swagger from "@/utils/swagger";
 function initApi(path) {
     axios.get(path)
         .then(function (response) {
-            console.debug(`[SDU] GET /swagger-resources : ${response.data}`)
-            store.commit('swaggerResources', response.data)
-            if (response.data) {
+            console.debug(`[SDU] GET /swagger-resources : ${response.data}`);
+            store.commit('swaggerResources', response.data);
+            if (response.data && response.data[0] && response.data[0].url) {
                 axios.get(response.data[0].url)
                     .then(function (swaggerResponse) {
-                        console.debug(`[SDU] GET ${response.data[0].url} : ${swaggerResponse.data}`)
+                        console.debug(`[SDU] GET ${response.data[0].url} : ${swaggerResponse.data}`);
                         store.commit('currentSwaggerJson', swagger.fixSwaggerJson(swaggerResponse.data))
                     })
             }
