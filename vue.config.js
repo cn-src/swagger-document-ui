6dc8242a388e309468191ebe4398c4c852f14cde
cn-src <public@javaer.cn>
2018-11-02 11:28:10 +0800

    update build 路径

--- vue.config.js
+++ vue.config.js
@@ -10,12 +10,13 @@ module.exports = {
             // 模板来源
             template: 'public/index.html',
             // 在 dist/index.html 的输出
-            filename: 'index.html',
+            filename: 'swagger-ui.html',
             // 当使用 title 选项时，
             // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
             title: 'API 文档'
         }
     },
+    assetsDir: 'swagger-document-ui',
     devServer: {
         proxy: 'http://swagger-bootstrap-ui.xiaominfo.com',
         before: function (app) {
