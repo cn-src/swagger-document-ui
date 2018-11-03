fc0cc1a944248220e6b6b5fffad8b5432e54bcf9
cn-src <public@javaer.cn>
2018-11-03 17:31:00 +0800

    add CopiedTag

---
+++ src/components/CopiedTag.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="copied-tag" @click="copiedToClipboard"><slot/></span>
+</template>
+
+<script>
+  import ClipboardJS from 'clipboard'
+    export default {
+        name: "CopiedTag",
+        methods: {
+            copiedToClipboard: function () {
+                new ClipboardJS('.copied-tag', {
+                    text: function(trigger) {
+                        return trigger.innerHTML;
+                    }
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  .copied-tag {
+
+  }
+</style>
