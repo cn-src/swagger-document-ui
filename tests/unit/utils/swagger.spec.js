66772da9c078d01a2b527c8b598113a12f6efbee
cn-src <public@javaer.cn>
2018-11-13 16:03:34 +0800

    update test

--- tests/unit/utils/swagger.spec.js
+++ tests/unit/utils/swagger.spec.js
@@ -3,10 +3,6 @@ import swagger from '@/utils/swagger'
 
 test('fixSwaggerJson', () => {
     const value = swagger.fixSwaggerJson(swaggerJson);
-    // console.log(JSON.stringify(value));
-    const entities = swagger.findSchemaEntities(value.collection['API注释'][0].parameters,value.definitions);
-    console.log(JSON.stringify(entities))
-
-
+    console.log(JSON.stringify(value));
 });
 
