a88553f8784fdd5e9346f93f9fb5d17f82536135
cn-src <public@javaer.cn>
2018-11-14 18:24:45 +0800

    开发搜索功能

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -2,14 +2,18 @@
   <AutoComplete
     v-model="keyword"
     @on-search="search"
+    clearable
     icon="ios-search"
     placeholder="搜索 API"
     style="width:300px">
     <div style="height: 300px;overflow-y: scroll;">
       <Option v-for="e in entities" :value="e.name" :key="e.id">
-        <MethodTag :method="e.method"/>
-        <span style="">{{ e.name }}</span><br>
-        <span style="color: #a6a9aa">{{ e.path }}</span>
+        <!-- AutoComplete 无法通过搜索 name 而拿到 id，故自定义点击事件-->
+        <div @click="select(e.id)">
+          <MethodTag :method="e.method"/>
+          <span style="">{{ e.name }}</span><br>
+          <span style="color: #a6a9aa">{{ e.path }}</span>
+        </div>
       </Option>
     </div>
   </AutoComplete>
@@ -32,6 +36,9 @@
         methods: {
             search(keyword) {
                 this.entities = _.slice(fuse.search(keyword), 0, 20)
+            },
+            select(entityId) {
+                this.$router.push(`/entity/${entityId}`)
             }
         }
     }
