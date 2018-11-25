83d8742b31b7766338783d7f233f63770ac1a547
cn-src <public@javaer.cn>
2018-11-25 20:23:12 +0800

    使用 pug 重构

--- src/router.js
+++ src/router.js
@@ -19,7 +19,7 @@ export default new Router({
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "entity-view" */ './views/EntityView.vue')
+            component: () => import(/* webpackChunkName: "entity-view" */ './views/entity-view')
         }
     ]
 })
