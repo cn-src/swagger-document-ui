5631fc52247e2f63d3d42fa0e928650c2ce7d5ae
cn-src <public@javaer.cn>
2018-11-25 19:51:10 +0800

    使用 pug 重构

--- src/views/entity-view/EntityViewResponse.vue
+++ src/views/entity-view/EntityViewResponse.vue
@@ -14,6 +14,16 @@
     export default {
         name: "EntityViewResponse",
         components: {EntityViewBean},
+        data() {
+            return {
+                responseBeanColumns: [
+                    {title: '状态', key: 'status', maxWidth: 62},
+                    {title: '描述', key: 'description'},
+                    {title: '类型', key: 'type', maxWidth: 100},
+                    {title: '格式', key: 'format', maxWidth: 150},
+                    {title: '约束', key: 'constraint', maxWidth: 250}]
+            }
+        },
         props: {
             httpEntity: {
                 type: Object,
