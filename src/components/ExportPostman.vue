65ab61a48b3a7237cbb3ea08350083ccd850bfd6
cn-src <public@javaer.cn>
2019-03-27 17:49:11 +0800

    实现 postman 格式导出功能

---
+++ src/components/ExportPostman.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+Modal(v-model='show' title='导出 Postman')
+    CellGroup(@on-click='')
+        Cell()
+</template>
+
+<script>
+export default {
+    name: 'ExportPostman',
+    data() {
+        return {
+            show: false
+        };
+    }
+};
+</script>
+<style>
+/* 修补压缩图标字体后造成的图标移位问题 */
+/*.ivu-modal .ivu-icon-ios-close {*/
+/*    top: -13px !important;*/
+/*}*/
+</style>
--- src/views/app-layout/HeaderContent.vue
+++ src/views/app-layout/HeaderContent.vue
@@ -5,33 +5,39 @@ Menu(mode='horizontal' theme='dark' @on-select='headerAction')
     MenuItem(name='swaggerResources')
         | {{ infoTitle }}&nbsp;
         Icon(type='md-repeat')
+    MenuItem(name='exportPostman')
+        | 导出 Postman
     div(style='float: right;margin-right: 20px;position: relative;')
         SearchInput
     SwaggerResources(ref='swaggerResources')
+    ExportPostman(ref='exportPostman')
 </template>
 
 <script>
-import SearchInput from '@/components/SearchInput'
-import SwaggerResources from '@/components/SwaggerResources'
+import SearchInput from '@/components/SearchInput';
+import SwaggerResources from '@/components/SwaggerResources';
+import ExportPostman from '@/components/ExportPostman';
 
 export default {
-    name: "HeaderContent",
-    components: {SearchInput, SwaggerResources},
+    name: 'HeaderContent',
+    components: { SearchInput, SwaggerResources, ExportPostman },
     methods: {
         headerAction(menuItemName) {
             if (menuItemName === 'swaggerResources') {
-                this.$refs.swaggerResources.show = true
+                this.$refs.swaggerResources.show = true;
+            } else if (menuItemName === 'exportPostman') {
+                this.$refs.exportPostman.show = true;
             }
         },
         collapsedSider() {
             this.$root.isCollapsed = !this.$root.isCollapsed;
-        },
+        }
     },
     computed: {
         infoTitle() {
             const currentSwaggerJson = this.$root.currentSwaggerJson;
-            return currentSwaggerJson && currentSwaggerJson.info && currentSwaggerJson.info.title || ''
+            return (currentSwaggerJson && currentSwaggerJson.info && currentSwaggerJson.info.title) || '';
         }
     }
-}
+};
 </script>
