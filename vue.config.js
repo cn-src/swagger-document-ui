fb0582d3770945c536ffbe6e199f3abc0f424442
cn-src <public@javaer.cn>
2019-03-30 20:58:20 +0800

    更新配置

--- vue.config.js
+++ vue.config.js
@@ -4,19 +4,7 @@ const swaggerJson = require('./tests/unit/utils/swagger.json');
 module.exports = {
     filenameHashing: false,
     publicPath: '',
-    pages: {
-        'swagger-document-ui': {
-            // page 的入口
-            entry: 'src/main.js',
-            // 模板来源
-            template: 'public/index.html',
-            // 在 dist/index.html 的输出
-            filename: 'swagger-ui.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-            title: 'API 文档'
-        }
-    },
+    indexPath: 'swagger-ui.html',
     assetsDir: 'webjars/swagger-document-ui',
     devServer: {
         openPage: '/swagger-ui.html',
