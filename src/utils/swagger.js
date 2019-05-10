e646f5142bcae89beb4535ec59d38716a7df3e00
cn-src <public@javaer.cn>
2019-05-10 16:49:51 +0800

    增加侧边栏菜单默认排序

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -39,7 +39,8 @@ function fixSwaggerJson(swaggerJson) {
             });
         });
     });
-    data.collection = $.groupBy(httpEntities, 'tag');
+
+    data.collection = $.groupBy($.sortBy(httpEntities, 'tag'), 'tag');
     return data;
 }
 
