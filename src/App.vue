6e753d6038a041247dd782db1f4c33299d66b530
cn-src <public@javaer.cn>
2019-05-29 21:32:16 +0800

    强制覆盖表格组件出现横向滚动条问题

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,48 @@
 <template lang="pug">
-Layout(stype="overflow-y:hide")
-    Header(:style="{padding: 0,position: 'fixed', width: '100%',zIndex:999}")
-        HeaderContent
-    Layout(:style="{height: '100vh', paddingTop:'64px', background: '#FFF'}")
-        Sider(
-        ref='side1' v-model='isCollapsed'
-        style='overflow: auto;'
-        breakpoint='md'
-        :width='270'
-        :collapsed-width='0'
-        hide-trigger
-        collapsible
-        )
-            SiderContent
-        Content(:style="{background: '#fff'}")
-            router-view
+    Layout(stype="overflow-y:hide")
+        Header(:style="{padding: 0,position: 'fixed', width: '100%',zIndex:999}")
+            HeaderContent
+        Layout(:style="{height: '100vh', paddingTop:'64px', background: '#FFF'}")
+            Sider(
+                ref='side1' v-model='isCollapsed'
+                style='overflow: auto;'
+                breakpoint='md'
+                :width='270'
+                :collapsed-width='0'
+                hide-trigger
+                collapsible
+            )
+                SiderContent
+            Content(:style="{background: '#fff'}")
+                router-view
 </template>
 <script>
-import HeaderContent from './views/app-layout/HeaderContent'
-import SiderContent from './views/app-layout/SiderContent'
+import HeaderContent from './views/app-layout/HeaderContent';
+import SiderContent from './views/app-layout/SiderContent';
 
 export default {
     name: 'App',
-    components: {HeaderContent, SiderContent},
+    components: { HeaderContent, SiderContent },
     data() {
         return {
-            isCollapsed: false,
-        }
+            isCollapsed: false
+        };
     },
     watch: {
-        '$root.isCollapsed': function () {
-            this.$refs.side1.toggleCollapse()
+        '$root.isCollapsed': function() {
+            this.$refs.side1.toggleCollapse();
         }
     }
-}
-
-
+};
 </script>
 <style lang="less">
 html {
     overflow-y: hidden;
+}
+
+.ivu-table-overflowX {
+    /*强制覆盖表格组件出现横向滚动条问题*/
+    overflow-x: hidden !important;
 }
 
 .ivu-menu-item {
