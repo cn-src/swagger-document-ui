0e252ffa37c2f875b973b9dc7e60aa6c19b74e84
cn-src <public@javaer.cn>
2019-05-29 15:19:50 +0800

    使用插槽语法重写部分代码

--- src/views/entity-view/EntityViewParam.vue
+++ src/views/entity-view/EntityViewParam.vue
@@ -1,25 +1,31 @@
 <template lang="pug">
 Table(
-:columns='paramBeanColumns'
-:data='httpEntity.paramBean.props'
-border size='small')
+    :columns='paramBeanColumns'
+    :data='httpEntity.paramBean.props'
+    border size='small')
+    template(slot-scope="{ row, index }" slot="name")
+        CopiedTag {{row.name}}
+        template(v-if="row.required === true")
+            span(style="color:red;") *
 </template>
 
 <script>
-import CopiedTag from '@/components/CopiedTag'
+import CopiedTag from '@/components/CopiedTag';
 
 export default {
     name: 'EntityViewParam',
+    components: { CopiedTag },
     data() {
         return {
             paramBeanColumns: [
-                {title: '名称', key: 'name', maxWidth: 150, render: paramNameRender},
-                {title: '描述', key: 'description'},
-                {title: '位置', key: 'in', maxWidth: 100, render: paramInRender},
-                {title: '类型', key: 'type', maxWidth: 100},
-                {title: '格式', key: 'format', maxWidth: 150},
-                {title: '约束', key: 'constraint', maxWidth: 250}],
-        }
+                { title: '名称', key: 'name', maxWidth: 150, slot: 'name' },
+                { title: '描述', key: 'description' },
+                { title: '位置', key: 'in', maxWidth: 100, render: paramInRender },
+                { title: '类型', key: 'type', maxWidth: 100 },
+                { title: '格式', key: 'format', maxWidth: 150 },
+                { title: '约束', key: 'constraint', maxWidth: 250 }
+            ]
+        };
     },
     props: {
         httpEntity: {
@@ -27,30 +33,23 @@ export default {
             required: true
         }
     }
-}
-
-function paramNameRender(h, params) {
-    if (params.row.required) {
-        return h('span', {}, [h(CopiedTag, {}, params.row.name), h('span', {
-            style: 'color:red;'
-        }, '*')])
-    } else {
-        return h(CopiedTag, {}, params.row.name)
-    }
-}
+};
 
 const inMapping = {
-    'header': '#ED4015',
-    'path': '#2D8CF0',
-    'query': '#18BE6B',
-    'body': '#FF9901',
+    header: '#ED4015',
+    path: '#2D8CF0',
+    query: '#18BE6B',
+    body: '#FF9901'
 };
 
 function paramInRender(h, params) {
     const color = inMapping[params.row.in] || '#7a7a7a';
-    return h('span', {
-        style: `color:${color};font-weight:bold;`
-    }, params.row.in)
+    return h(
+        'span',
+        {
+            style: `color:${color};font-weight:bold;`
+        },
+        params.row.in
+    );
 }
 </script>
-
